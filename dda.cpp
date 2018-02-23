b7cfbd11db172232515e7f935c5d6628c5cdb9d7
Unknown <anjali.mca17.du@gmail.com>
2018-02-23 20:21:16 +0530

    Documentation

--- dda.cpp
+++ dda.cpp
@@ -1,5 +1,6 @@
 /*
-  PROGRAM TO IMPLEMENT DDA ALGORITHM
+    PROGRAM TO IMPLEMENT DDA LINE ALGORITHM
+    WRITTEN BY: ANJALI BANSAL
 */
 
 #include<iostream.h>
@@ -7,35 +8,37 @@
 #include<graphics.h>
 #include<math.h>
 
-void dda(int x0, int y0, int x1, int y1) {
-  int dx = x1-x0;
-  int dy = y1-y0;
-  double m=dy/dx;
-  double y=y0;
-  int x;
+void ddaLine(int x0, int y0, int x1, int y1) {
+    int dx = x1-x0;
+    int dy = y1-y0;
+    double m=dy/dx;
+    double y=y0;
+    int x;
 
-  for(x=x0;x<=x1;x++)
-  {
-    putpixel(x,y,RED);
-    y+=m;
-  }
+    for(x=x0;x<=x1;x++) {
+        putpixel(x,y,RED);
+        y+=m;
+    }
 }
 
+
 int main() {
+    clrscr();
+    int graphdriver = DETECT;
+    int graphmode;
+    int x0,y0,x1,y1;
 
-  int graphdriver = DETECT;
-  int graphmode;
-  initgraph(&graphdriver, &graphmode, "C:\\TURBOC3\\BGI");
+    cout<<"\n *** DDA Line Algorithm *** ";
+    cout<<"\n\n Enter the first point coordinates(x0,y0): ";
+    cin>>x0>>y0;
+    cout<<"\n Enter the second point coordinates(x1,y1): ";
+    cin>>x1>>y1;
 
-  int x0,y0,x1,y1;
-  cout<<"Enter the first coordinate:";
-  cin>>x0>>y0;
-  cout<<"Enter the second coordinate:";
-  cin>>x1>>y1;
-  dda(x0,y0,x1,y1);
+    initgraph(&graphdriver, &graphmode, "C:\\TURBOC3\\BGI");
+    ddaLine(x0,y0,x1,y1);
 
-  getch();
-  closegraph();
-  restorecrtmode();
-  return 0;
+    getch();
+    closegraph();
+    restorecrtmode();
+    return 0;
 }
